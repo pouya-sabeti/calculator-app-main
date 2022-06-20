7c09b1dfce53494eda6bb0c59e2cea37928698ec
Pouya sabeti <pouyasabeti.sgs@gmail.com>
2022-06-20 14:11:24 +0430

    complet CHALLENGE

--- js/app.js
+++ js/app.js
@@ -17,3 +17,74 @@ function change(a, b) {
         document.body.className = "thme-3";
     }
 }
+var value_1 = "";
+var value_3 = "";
+function calcul(a) {
+    if (!(document.querySelector(".monitor-input").value.length >= 23)) {
+        document.querySelector(".monitor-input").value += a;
+    }
+
+
+
+}
+function check() {
+    if (document.querySelector(".monitor-input").value.length < 19 && document.querySelector(".monitor-input").value.length > 14) {
+        document.querySelector(".monitor-input").classList.remove("font_size_50");
+        document.querySelector(".monitor-input").classList.remove("font_size_30");
+        document.querySelector(".monitor-input").classList.add("font_size_35");
+    }
+    if (document.querySelector(".monitor-input").value.length < 14) {
+        document.querySelector(".monitor-input").classList.remove("font_size_35");
+        document.querySelector(".monitor-input").classList.add("font_size_50");
+    }
+    if (document.querySelector(".monitor-input").value.length > 19) {
+        document.querySelector(".monitor-input").classList.remove("font_size_35");
+
+        document.querySelector(".monitor-input").classList.add("font_size_30");
+    }
+    if (document.querySelector(".monitor-input").value.length == 23) {
+        document.querySelector(".error").classList.add("show_error");
+    }
+    else {
+        document.querySelector(".error").classList.remove("show_error");
+    }
+
+}
+setInterval(check, 100)
+function math_a(a) {
+    if (!(value_3 == "")) {
+        value_1 = "";
+    }
+    value_1 = value_1 + document.querySelector(".monitor-input").value + a;
+    console.log(value_1);
+    document.querySelector(".math_input").innerHTML = value_1;
+    document.querySelector(".monitor-input").value = "";
+}
+function Equal() {
+    value_1 = value_1 + document.querySelector(".monitor-input").value;
+    document.querySelector(".math_input").innerHTML = value_1;
+    document.querySelector(".monitor-input").value = "";
+    var value_2 = document.querySelector(".math_input").innerHTML;
+
+    document.querySelector(".monitor-input").value = value_3 = eval(value_2);
+
+}
+
+function del() {
+    var value_4 = document.querySelector(".monitor-input").value.split("");
+    value_4.pop();
+    var fg = "";
+    for (let i = 0; i < value_4.length; i++) {
+        fg += value_4[i];
+
+    }
+    document.querySelector(".monitor-input").value = fg;
+}
+
+function reset() {
+    document.querySelector(".monitor-input").value = "";
+    document.querySelector(".math_input").innerHTML = "Calculator";
+    value_1 = "";
+    value_2 = "";
+    value_3 = "";
+}
